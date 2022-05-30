b40614976711546370641c7114221192430e5337
yeji <kim1070720@naver.com>
2022-05-30 23:18:22 +0900

    영화 정보의 수 추가

---
+++ app/src/main/java/com/example/a11st/MovieList.java
@@ -0,0 +1,5 @@
+package com.example.a11st;
+
+public class MovieList {
+    MovieListResult boxOfficeResult;
+}
---
+++ app/src/main/java/com/example/a11st/MovieListResult.java
@@ -0,0 +1,11 @@
+package com.example.a11st;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+}
