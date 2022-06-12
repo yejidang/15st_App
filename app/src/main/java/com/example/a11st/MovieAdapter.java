9044df6fc324a4175bc6bd544d8af7594550b322
yeji <kim1070720@naver.com>
2022-06-13 01:30:05 +0900

    기말고사

--- app/src/main/java/com/example/a11st/MovieAdapter.java
+++ app/src/main/java/com/example/a11st/MovieAdapter.java
@@ -33,19 +33,29 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     public void setItems (ArrayList < Movie > items) { this.items = items; }
     public Movie getItem ( int position){ return items.get(position); }
 
-        static class ViewHolder extends RecyclerView.ViewHolder {
-            TextView moviewName;
-            TextView peopleCount;
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView moviewName;
+        TextView peopleCount;
+        TextView openMovie;
+        TextView movieRank;
+        TextView tdpeopleCount;
 
-            public ViewHolder(View itemView) {
-                super(itemView);
-                moviewName = itemView.findViewById(R.id.movieName);
-                peopleCount = itemView.findViewById(R.id.peopleCount);
-            }
 
-            public void setItem(Movie item) {
-                moviewName.setText(item.movieNm);
-                peopleCount.setText(item.audiCnt);
-            }
+        public ViewHolder(View itemView) {
+            super(itemView);
+            moviewName = itemView.findViewById(R.id.movieName);
+            peopleCount = itemView.findViewById(R.id.peopleCount);
+            openMovie = itemView.findViewById(R.id.openMovie);
+            movieRank = itemView.findViewById(R.id.movieRank);
+            tdpeopleCount = itemView.findViewById(R.id.tdpeopleCount);
+        }
+
+        public void setItem(Movie item) {
+            moviewName.setText(item.movieNm);
+            peopleCount.setText("누적관객수 : " + item.audiAcc + "명");
+            openMovie.setText("개봉일 : " + item.openDt);
+            movieRank.setText(item.rank + "위");
+            tdpeopleCount.setText("해당일의 관객수 : " + item.audiCnt + "명");
         }
     }
+}
